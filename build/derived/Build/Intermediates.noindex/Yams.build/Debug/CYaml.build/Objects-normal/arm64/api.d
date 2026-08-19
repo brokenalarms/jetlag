00df63e5d60536ad115a2494cda8d26fb815de24
brokenalarms <brokenalarms@users.noreply.github.com>
2026-08-19 08:59:58 -0700

    remove build

--- build/derived/Build/Intermediates.noindex/Yams.build/Debug/CYaml.build/Objects-normal/arm64/api.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/daniel/Developer/jetlag/.ralph/worktrees/ralph-20260819-01/build/derived/SourcePackages/checkouts/Yams/Sources/CYaml/include/module.modulemap \
-  /Users/daniel/Developer/jetlag/.ralph/worktrees/ralph-20260819-01/build/derived/SourcePackages/checkouts/Yams/Sources/CYaml/src/api.c \
-  /Users/daniel/Developer/jetlag/.ralph/worktrees/ralph-20260819-01/build/derived/SourcePackages/checkouts/Yams/Sources/CYaml/src/yaml_private.h \
-  /Users/daniel/Developer/jetlag/.ralph/worktrees/ralph-20260819-01/build/derived/SourcePackages/checkouts/Yams/Sources/CYaml/include/yaml.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/DarwinFoundation3.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/DarwinFoundation2.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/DarwinFoundation1.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/21/include/module.modulemap
